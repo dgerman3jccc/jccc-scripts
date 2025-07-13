432e5b64537d302da358d88354d1fdf608b71e8e
Damon German <49420596+dgerman3@users.noreply.github.com>
2025-07-13 15:07:18 -0500

    Initial clean commit for branch 'animal-sounds-polymorphism-inclass'

---
+++ cs236-in-class/main/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace main;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        Animal[] animals =
+        {
+            new Dog(), new Cat()
+        };
+
+        // PrintAnimalSounds(animals);
+    }
+
+    // TODO: add PrintAnimalSounds method here
+}
