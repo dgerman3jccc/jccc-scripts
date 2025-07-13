9fa6122dd2a57c674e59f8dbbddd1e1b4053e1e2
Damon German <49420596+dgerman3@users.noreply.github.com>
2025-07-13 15:07:21 -0500

    Initial clean commit for branch 'animal-sounds-polymorphism-solution'

---
+++ cs236-in-class/main/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace main;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        Animal[] animals =
+        {
+            new Dog(), new Cat()
+        };
+
+        PrintAnimalSounds(animals);
+    }
+
+    private static void PrintAnimalSounds(Animal[] animals)
+    {
+        foreach (Animal animal in animals)
+        {
+            Console.WriteLine("This animal makes: " + animal.MakeSound());
+        }
+    }
+}
